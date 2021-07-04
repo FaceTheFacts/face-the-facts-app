c5070ab4a192a2e4082ca4e756b2231ea57a8e67
Maximilian Schelbach <6402841+maximilianschelbach@users.noreply.github.com>
2021-07-04 19:42:41 +0200

    set iOS status bar to match content of the app

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import MainView from './view/MainView';
 import EmbeddedView from './view/EmbeddedView';
+import {StatusBar} from 'react-native';
 
 const data = new FaceTheFactsData({
   politicians,
@@ -19,6 +20,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <DataContext.Provider value={data}>
+      <StatusBar barStyle="light-content" />
       <NavigationContainer>
         <Stack.Navigator headerMode="none">
           <Stack.Screen name="main" component={MainView} />
