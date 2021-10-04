4cf6067c2f7393ce5e026a9949528b74fd13aba0
Kevin Gao <k_gao@aol.com>
2021-10-04 16:50:48 +0200

    refactor: remove code related to PoliticianModal from PoliticianRow

--- src/component/PoliticianRow.tsx
+++ src/component/PoliticianRow.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useRef} from 'react';
+import React, {useContext} from 'react';
 import {DataContext} from '../logic/model';
 import {
   StyleProp,
@@ -12,8 +12,6 @@ import {Colors} from '../theme';
 import PartyTag from './PartyTag';
 import PoliticianPicture from './PoliticianPicture';
 import {Politician} from '../logic/data';
-import {Modalize} from 'react-native-modalize';
-import PoliticianModal from '../view/PoliticianModal';
 import {NavigationContext} from '@react-navigation/native';
 
 export interface PoliticianRowProps {
@@ -28,29 +26,23 @@ const PoliticianRow = ({
   parentPoliticianId,
 }: PoliticianRowProps) => {
   const data = useContext(DataContext);
-  const modal = useRef<Modalize>(null);
-  const navigator = useContext(NavigationContext)!;
+  const navigator = useContext<any>(NavigationContext)!;
 
   return (
-    <>
-      <TouchableOpacity
-        style={StyleSheet.flatten([styles.container, style])}
-        onPress={() => {
-          if (politician.id !== parentPoliticianId) {
-            navigator.push('politician', {
-              politician,
-            });
-            data.historyManager.pushItem(politician.id);
-          }
-        }}>
-        <PoliticianPicture politicianId={politician.id} />
-        <View style={styles.content}>
-          <Text style={styles.name}>{politician.name}</Text>
-          <PartyTag party={data.lookupParty(politician.partyId)!} />
-        </View>
-      </TouchableOpacity>
-      <PoliticianModal modalRef={modal} politician={politician} />
-    </>
+    <TouchableOpacity
+      style={StyleSheet.flatten([styles.container, style])}
+      onPress={() => {
+        if (politician.id !== parentPoliticianId) {
+          navigator.push('PoliticianScreen', {politician});
+          data.historyManager.pushItem(politician.id);
+        }
+      }}>
+      <PoliticianPicture politicianId={politician.id} />
+      <View style={styles.content}>
+        <Text style={styles.name}>{politician.name}</Text>
+        <PartyTag party={data.lookupParty(politician.partyId)!} />
+      </View>
+    </TouchableOpacity>
   );
 };
 
