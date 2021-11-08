5eeb9bea02dd34de7eb27f7087ca6b3357226222
Kevin Gao <k_gao@aol.com>
2021-11-08 19:48:40 +0100

    ci: add eslint plus action

---
+++ src/view/HomeView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const HomeView = () => {
+  return (
+    <SafeAreaView>
+      <Text>kdjaslkdj</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeView;
