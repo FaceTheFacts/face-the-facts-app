3ca3b805c4e7637ebaf4c3b2047dd592c62c85ee
Richard <richard.kruemmel@code.berlin>
2021-11-12 14:27:00 +0100

    Connect to the API

--- src/logic/fetch.ts
+++ src/logic/fetch.ts
@@ -1,8 +1,9 @@
 export async function fetch_data(url: string) {
-    const BASE_URL = 'http://127.0.0.1:8000/';
+    const BASE_URL = 'https://api.facethefacts-api.de/';
     try {
 	const response = await fetch(BASE_URL + url);
     const json = await response.json()
+    console.log(url)
 	return json;
     } catch(error) {
         console.error(error);
