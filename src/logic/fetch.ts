960a84e204366e96d10602dd332038893534182c
Richard <richard.kruemmel@code.berlin>
2021-12-06 14:09:33 +0100

    Implement better error handling

--- src/logic/fetch.ts
+++ src/logic/fetch.ts
@@ -1,11 +1,11 @@
-export async function fetch_data(url: string) {
-    const BASE_URL = 'https://api.facethefacts-api.de/';
-    try {
-	const response = await fetch(BASE_URL + url);
-    const json = await response.json()
-    console.log(url)
-	return json;
-    } catch(error) {
-        console.error(error);
+export async function fetch_api<T>(url: string): Promise<T | undefined> {
+  const BASE_URL = 'https://api.facethefacts-api.de/v1';
+  try {
+    const response = await fetch(`${BASE_URL}/${url}`);
+    if (response.status >= 200 && response.status <= 299) {
+      return await response.json();
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
