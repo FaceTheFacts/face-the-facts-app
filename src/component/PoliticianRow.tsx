3d7906f51a0d132a903a5b6501f38e51a1fe37b1
Richard <richard.kruemmel@code.berlin>
2021-12-07 11:59:51 +0100

    Implement onClick PoliticianProfileData fetch.

--- src/component/PoliticianRow.tsx
+++ src/component/PoliticianRow.tsx
@@ -11,13 +11,15 @@ import {
 import {Colors} from '../theme';
 import PartyTag from './PartyTag';
 import PoliticianPicture from './PoliticianPicture';
-import {Politician, SearchedPolitician} from '../logic/data';
 import {NavigationContext} from '@react-navigation/native';
+import {useQuery} from 'react-query';
+import {ApiPolitician, ApiSearchPolitician} from '../logic/api';
+import {fetch_api} from '../logic/fetch';
 
 export interface PoliticianRowProps {
   style?: StyleProp<ViewStyle>;
-  politician: SearchedPolitician;
-  parentPoliticianId?: string;
+  politician: ApiSearchPolitician;
+  parentPoliticianId?: number;
 }
 
 const PoliticianRow = ({
@@ -25,22 +27,35 @@ const PoliticianRow = ({
   politician,
   parentPoliticianId,
 }: PoliticianRowProps) => {
-  const data = useContext(DataContext);
+  const database = useContext(DataContext);
   const navigator = useContext<any>(NavigationContext)!;
 
+  const handleClick = () => {
+    refetch();
+  };
+
+  const {data, status, refetch} = useQuery<ApiPolitician | undefined, Error>(
+    `politician-${politician.id}`,
+    () =>
+      fetch_api<ApiPolitician>(
+        `politician/${politician.id}?sidejobs_end=15&votes_end=5`,
+      ),
+    {enabled: false},
+  );
+  if (status === 'success' && data !== undefined) {
+    navigator.push('PoliticianScreen', data);
+    database.historyManager.pushItem(politician.id);
+  }
   return (
     <TouchableOpacity
       style={StyleSheet.flatten([styles.container, style])}
       onPress={() => {
-        if (politician.id !== parentPoliticianId) {
-          navigator.push('PoliticianScreen', {politician});
-          data.dbManager.pushHistoryItem(politician.id);
-        }
+        handleClick();
       }}>
       <PoliticianPicture politicianId={politician.id} />
       <View style={styles.content}>
-        <Text style={styles.name}>{politician.name}</Text>
-        {/* <PartyTag party={data.lookupParty(politician.partyId)!} /> */}
+        <Text style={styles.name}>{politician.label}</Text>
+        <PartyTag party={politician.party} />
       </View>
     </TouchableOpacity>
   );
