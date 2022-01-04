52818cb870b8bddbe1b567e78d818ed3085e929f
Richard <richard.kruemmel@code.berlin>
2022-01-04 14:48:56 +0100

    Change naming of vote from none to no_show

--- src/component/utils/VoteTag.tsx
+++ src/component/utils/VoteTag.tsx
@@ -7,14 +7,14 @@ export const voteLabels: Record<Vote, string> = {
   yes: 'Ja',
   no: 'Nein',
   abstain: 'Enthalten',
-  none: 'Abwesend',
+  no_show: 'Abwesend',
 };
 
 export const voteColors: Record<Vote, string> = {
   yes: '#5AB760',
   no: '#BB3C45',
   abstain: 'rgba(57, 105, 245, 0.3)',
-  none: 'rgba(248, 248, 248, 0.12)',
+  no_show: 'rgba(248, 248, 248, 0.12)',
 };
 
 export interface VoteTagProps {
