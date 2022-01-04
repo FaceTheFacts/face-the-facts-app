301965afe32ac1d97b6185a91e6d883f962e5b70
Richard <richard.kruemmel@code.berlin>
2022-01-04 14:43:06 +0100

    Add position query

--- src/component/PoliticianRow.tsx
+++ src/component/PoliticianRow.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {DataContext} from '../logic/model';
 import {
   StyleProp,
@@ -13,38 +13,53 @@ import PartyTag from './PartyTag';
 import PoliticianPicture from './PoliticianPicture';
 import {NavigationContext} from '@react-navigation/native';
 import {useQuery} from 'react-query';
-import {ApiPolitician, ApiSearchPolitician} from '../logic/api';
+import {
+  ApiPoliticianProfile,
+  ApiPositions,
+  ApiSearchPolitician,
+} from '../logic/api';
 import {fetch_api} from '../logic/fetch';
 
 export interface PoliticianRowProps {
   style?: StyleProp<ViewStyle>;
   politician: ApiSearchPolitician;
-  parentPoliticianId?: number;
 }
 
-const PoliticianRow = ({
-  style,
-  politician,
-  parentPoliticianId,
-}: PoliticianRowProps) => {
+const PoliticianRow = ({style, politician}: PoliticianRowProps) => {
   const database = useContext(DataContext);
   const navigator = useContext<any>(NavigationContext)!;
+  const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
-    refetch();
+    profileQuery.refetch();
+    positionsQuery.refetch();
+    setClicked(true);
   };
 
-  const {data, status, refetch} = useQuery<ApiPolitician | undefined, Error>(
+  const profileQuery = useQuery<ApiPoliticianProfile | undefined, Error>(
     `politician-${politician.id}`,
     () =>
-      fetch_api<ApiPolitician>(
+      fetch_api<ApiPoliticianProfile>(
         `politician/${politician.id}?sidejobs_end=15&votes_end=5`,
       ),
     {enabled: false},
   );
-  if (status === 'success' && data !== undefined) {
-    navigator.push('PoliticianScreen', data);
+  const positionsQuery = useQuery<ApiPositions | undefined, Error>(
+    `postition-${politician.id}`,
+    () => fetch_api<ApiPositions>(`politician/${politician.id}/positions`),
+    {enabled: false},
+  );
+  if (
+    profileQuery.status === 'success' &&
+    positionsQuery.status === 'success' &&
+    clicked
+  ) {
+    navigator.push('PoliticianScreen', {
+      profile: profileQuery.data,
+      positions: positionsQuery.data,
+    });
     database.historyManager.pushItem(politician.id);
+    setClicked(false);
   }
   return (
     <TouchableOpacity
