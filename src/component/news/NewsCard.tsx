75685465ce0d5e95fe8e89117436d5a2e866602a
Richard <richard.kruemmel@code.berlin>
2022-01-12 23:29:12 +0100

    Create NewsCard component

---
+++ src/component/news/NewsCard.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Linking} from 'react-native';
+import {PoliTrackImage} from '../../logic/api';
+import {Colors} from '../../theme';
+import NewsPicture from './NewsPicture';
+
+interface NewsCardProps {
+  title: string;
+  date: string;
+  image: PoliTrackImage[];
+  url: string;
+  source: string;
+}
+
+const NewsCard = ({title, date, image, url, source}: NewsCardProps) => {
+  return (
+    <TouchableOpacity onPress={() => Linking.openURL(url!)}>
+      <View style={styles.container}>
+        <NewsPicture source={source} image={image} />
+        <View style={styles.cardContent}>
+          <Text style={styles.titleText}>{title}</Text>
+          <Text style={styles.dateText}>{date}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 191,
+    height: 200,
+    borderRadius: 8,
+    backgroundColor: Colors.cardBackground,
+    marginRight: 8,
+  },
+  cardContent: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  titleText: {
+    paddingTop: 12,
+    paddingHorizontal: 12,
+    fontSize: 13,
+    lineHeight: 15.73,
+    color: Colors.baseWhite,
+  },
+  dateText: {
+    paddingBottom: 12,
+    paddingHorizontal: 12,
+    fontSize: 11,
+    lineHeight: 13.31,
+    color: Colors.white70,
+    alignSelf: 'flex-end',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    color: Colors.background,
+  },
+});
+
+export default NewsCard;
