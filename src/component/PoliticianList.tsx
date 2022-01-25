58aa1da4eb4dc3c69533767ba5e64c3905adccc7
Richard <richard.kruemmel@code.berlin>
2022-01-25 21:09:29 +0100

    Delete Date section for HistoryView

--- src/component/PoliticianList.tsx
+++ src/component/PoliticianList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import {Colors} from '../theme';
+import {ScrollView, StyleSheet} from 'react-native';
 import PoliticianRow from './PoliticianRow';
 
 export interface PoliticianListSection {
@@ -15,39 +14,22 @@ export interface PoliticianListProps {
 const PoliticianList = ({sections}: PoliticianListProps) => {
   return (
     <ScrollView>
-      {sections.map(({title, politicianIds}, index) => (
-        <View key={index} style={styles.section}>
-          <Text style={styles.header}>{title}</Text>
-          {politicianIds.map(politicianId => (
-            <PoliticianRow
-              key={politicianId}
-              style={styles.politician}
-              politicianId={politicianId}
-            />
-          ))}
-        </View>
-      ))}
+      {sections.map(({politicianIds}) =>
+        politicianIds.map(politicianId => (
+          <PoliticianRow
+            key={politicianId}
+            style={styles.politician}
+            politicianId={politicianId}
+          />
+        )),
+      )}
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  section: {
-    marginBottom: 8,
-  },
-  header: {
-    fontFamily: 'Inter',
-    fontSize: 18,
-    color: Colors.foreground,
-    opacity: 0.7,
-    marginBottom: 10,
-    marginLeft: 16,
-    marginRight: 16,
-  },
   politician: {
     marginBottom: 10,
-    marginLeft: 16,
-    marginRight: 16,
   },
 });
 
