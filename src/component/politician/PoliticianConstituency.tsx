ae64f46d1b40208c6e8a4a38b018ccdf4a5da090
Richard <richard.kruemmel@code.berlin>
2022-01-28 12:10:42 +0100

    Delete SafeAreaView bottom

--- src/component/politician/PoliticianConstituency.tsx
+++ src/component/politician/PoliticianConstituency.tsx
@@ -3,32 +3,24 @@ import {StyleSheet, View} from 'react-native';
 import {PoliticianContext} from '../../view/PoliticianView';
 import {Colors} from '../../theme';
 import {ScrollView} from 'react-native-gesture-handler';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import PoliticianRow from '../PoliticianRow';
 
 const PoliticianConstituency = () => {
   const politician = useContext(PoliticianContext);
   return (
-    <>
-      <View style={styles.container}>
-        <ScrollView>
-          {politician?.constituency &&
-            politician?.constituency.map(
-              (constituencyCandidate, index) =>
-                constituencyCandidate.id !== politician.profile?.id && (
-                  <View style={styles.rowContainer}>
-                    <PoliticianRow
-                      key={index}
-                      politician={constituencyCandidate}
-                      politicianId={constituencyCandidate.id}
-                    />
-                  </View>
-                ),
-            )}
-        </ScrollView>
-      </View>
-      <SafeAreaView style={styles.iosSafeBottom} />
-    </>
+    <View style={styles.container}>
+      <ScrollView>
+        {politician?.constituency &&
+          politician?.constituency.map(
+            (constituencyCandidate, index) =>
+              constituencyCandidate.id !== politician.profile?.id && (
+                <View style={styles.rowContainer} key={index}>
+                  <PoliticianRow politicianId={constituencyCandidate.id} />
+                </View>
+              ),
+          )}
+      </ScrollView>
+    </View>
   );
 };
 
@@ -36,6 +28,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 6,
+    paddingBottom: 12,
     paddingHorizontal: 12,
   },
   rowContainer: {
