8b47ebf6a6b795cbb2ab66beeb08f7a4203d2849
Richard <richard.kruemmel@code.berlin>
2022-02-11 13:53:02 +0100

    1st attempt of fixing modal bug

--- src/component/utils/BottomSheet.tsx
+++ src/component/utils/BottomSheet.tsx
@@ -1,7 +1,8 @@
 import React, {Ref, useState} from 'react';
-import {SafeAreaView, useWindowDimensions} from 'react-native';
+import {useWindowDimensions, View} from 'react-native';
 import {Modalize, ModalizeProps} from 'react-native-modalize';
 import {Portal} from 'react-native-portalize';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Colors} from '../../theme';
 
 export interface ModalSheetProps extends ModalizeProps {
@@ -21,6 +22,8 @@ const BottomSheet = ({
 
   const [autoOpened, setAutoOpened] = useState(false);
   const {width} = useWindowDimensions();
+  const insets = useSafeAreaInsets();
+  const marginBottom = insets.bottom;
 
   return (
     <Portal>
@@ -39,7 +42,9 @@ const BottomSheet = ({
         scrollViewProps={{horizontal: true, scrollEnabled: false}}
         {...modalizeProps}
         FooterComponent={
-          <SafeAreaView style={{width}}>{children}</SafeAreaView>
+          <View style={{width: width, marginBottom: marginBottom}}>
+            {children}
+          </View>
         }
       />
     </Portal>
