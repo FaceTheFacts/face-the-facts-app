c3796b7e4148b8cda5d9213ca3688aec6afe1feb
Richard <richard.kruemmel@code.berlin>
2022-02-11 16:04:19 +0100

    Add route params

--- src/view/RootStackParams.ts
+++ src/view/RootStackParams.ts
@@ -1,4 +1,11 @@
-import {ApiParty, IPoliticianContext} from '../logic/api';
+import {
+  ApiParty,
+  ApiPoliticianProfile,
+  ApiPoll,
+  ApiVote,
+  IPoliticianContext,
+} from '../logic/api';
+import {Vote} from '../logic/data';
 
 //to do: Define the params of the other routes
 export type RootStackParamList = {
@@ -9,8 +16,13 @@ export type RootStackParamList = {
     party: ApiParty;
   };
   Politician: {politicianId: number};
-  News: undefined;
-  Speeches: undefined;
+  News: {politician: IPoliticianContext};
+  Speeches: {politician: IPoliticianContext};
   Polls: {politician: IPoliticianContext};
-  PollDetails: undefined;
+  PollDetails: {
+    poll: ApiPoll;
+    vote: ApiVote;
+    candidateVote: Vote;
+    politician: ApiPoliticianProfile;
+  };
 };
