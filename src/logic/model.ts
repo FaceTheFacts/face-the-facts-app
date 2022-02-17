a30455c72cdcd74f7596fcdd94cc7ccbda390ddb
Kevin Gao <k_gao@aol.com>
2022-02-17 22:04:56 +0100

    restore mode.ts

--- src/logic/model.ts
+++ src/logic/model.ts
@@ -44,10 +44,10 @@ export class FaceTheFactsData {
     onData: (data: FaceTheFactsData) => void,
     onNoData: () => void,
   ): Promise<void> {
-    // if (__DEV__) {
-    //   onData(new FaceTheFactsData(require('../../data.json')));
-    //   return;
-    // }
+    if (__DEV__) {
+      onData(new FaceTheFactsData(require('../../data.json')));
+      return;
+    }
 
     const headers: Headers = {};
     let fileAlreadyExists = false;
