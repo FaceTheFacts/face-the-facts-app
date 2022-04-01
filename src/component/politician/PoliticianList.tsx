eb2d4bbaddf8270e955ae85a3894b6fc07f7826e
Richard <richard.kruemmel@code.berlin>
2022-04-01 16:35:17 +0200

    Add ErrorCard and change Datatype

--- src/component/politician/PoliticianList.tsx
+++ src/component/politician/PoliticianList.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
-import {Text} from 'react-native-svg';
 import {useQuery} from 'react-query';
-import {ApiSearchPolitician} from '../../logic/api';
+import {ApiPoliticianHeader} from '../../logic/api';
 import {fetch_api} from '../../logic/fetch';
 import PoliticianItem from './PoliticianItem';
 import SkeletonPoliticianItem from '../skeleton/SkeletonPoliticianItem';
+import ErrorCard from '../Error';
 
 export interface PoliticianListProps {
   politicianIds: number[];
@@ -15,14 +15,13 @@ const PoliticianList = ({politicianIds}: PoliticianListProps) => {
   const history = politicianIds.reverse();
   const politicianHistory =
     history.length > 10 ? history.slice(0, 10) : history;
-
   let query = '';
   politicianHistory.forEach(politicianId => {
     query += `ids=${politicianId}&`;
   });
-  const historyQuery = useQuery<ApiSearchPolitician[] | undefined, Error>(
+  const historyQuery = useQuery<ApiPoliticianHeader[] | undefined, Error>(
     `politicianshistory:${query}`,
-    () => fetch_api<ApiSearchPolitician[]>(`politicianshistory?${query}`),
+    () => fetch_api<ApiPoliticianHeader[]>(`politicianshistory/?${query}`),
     {
       staleTime: 60 * 10000000, // 10000 minute = around 1 week
       cacheTime: 60 * 10000000,
@@ -34,7 +33,7 @@ const PoliticianList = ({politicianIds}: PoliticianListProps) => {
   }
 
   if (historyQuery.isError) {
-    return <Text>Error ...</Text>;
+    return <ErrorCard />;
   }
 
   return (
