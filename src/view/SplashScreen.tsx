aa586a386e133c800ab3881a080b29b0f7116162
Richard <richard.kruemmel@code.berlin>
2022-04-01 11:26:07 +0200

    Update Splashscreen

--- src/view/SplashScreen.tsx
+++ src/view/SplashScreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Image, StyleSheet, useWindowDimensions, View} from 'react-native';
+import {Image, StyleSheet, Text, useWindowDimensions, View} from 'react-native';
 import {Colors} from '../theme';
 import splash from '../splash.png';
-import poweredByAbgeordnetenwatch from '../powered_by_abgeordnetenwatch.png';
+import poliTrack from '../poliTrack.png';
+import openParliamentTV from '../openParliamentTV.png';
+import abgeordnetenwatch from '../abgeordnetenwatch.png';
 
 const SplashScreen = () => {
   const {width, height} = useWindowDimensions();
+  console.log(width);
 
   const styles = StyleSheet.create({
     container: {
@@ -14,18 +17,45 @@ const SplashScreen = () => {
       width,
       height,
     },
+    text: {
+      fontFamily: 'Inter',
+      fontSize: 11,
+      alignSelf: 'center',
+      color: 'rgba(248, 248, 248, 0.7)',
+      marginBottom: 12,
+    },
     splash: {
       position: 'absolute',
       alignSelf: 'center',
       width: width - 150,
       height: ((width - 150) / 848) * 160,
     },
-    poweredByAbgeordnetenwatch: {
+    partners: {
       position: 'absolute',
+      alignSelf: 'center',
+      bottom: 80,
+    },
+    firstRow: {
+      flexDirection: 'row',
+    },
+    poliTrack: {
+      alignSelf: 'center',
+      width: width - 300,
+      height: ((width - 180) / 800) * 140,
+      resizeMode: 'contain',
+      marginRight: 12,
+    },
+    openParliamentTV: {
+      alignSelf: 'center',
+      width: width - 280,
+      height: ((width - 180) / 800) * 140,
+      resizeMode: 'contain',
+      marginLeft: 12,
+    },
+    abgeordnetenwatch: {
       alignSelf: 'center',
       width: width - 180,
       height: ((width - 180) / 800) * 140,
-      bottom: 50,
       resizeMode: 'contain',
     },
   });
@@ -33,10 +63,15 @@ const SplashScreen = () => {
   return (
     <View style={styles.container}>
       <Image style={styles.splash} source={splash} />
-      <Image
-        style={styles.poweredByAbgeordnetenwatch}
-        source={poweredByAbgeordnetenwatch}
-      />
+      <View style={styles.partners}>
+        <Text style={styles.text}>powered by</Text>
+        <View style={styles.firstRow}>
+          <Image style={styles.poliTrack} source={poliTrack} />
+          <Image style={styles.openParliamentTV} source={openParliamentTV} />
+        </View>
+
+        <Image style={styles.abgeordnetenwatch} source={abgeordnetenwatch} />
+      </View>
     </View>
   );
 };
