9e0ce06412bd81c1aaedbec1f99edbdd9f6cd8f4
Richard <richard.kruemmel@code.berlin>
2022-12-13 12:49:27 +0100

    Extract fetching into own useFetch function

---
+++ src/queries/useFetchDashboard.ts
@@ -0,0 +1,54 @@
+import {fetch_api} from '../logic/fetch';
+import {
+  ApiPaginatedData,
+  ApiSpeechBundestag,
+  ApiSidejobsBundestag,
+  ApiPollBundestagData,
+  ApiBundestagPartyDonation,
+} from '../logic/api';
+import {useQueries} from 'react-query';
+
+const queryFunctions = {
+  speeches: () =>
+    fetch_api<ApiPaginatedData<ApiSpeechBundestag>>(
+      'bundestag/speeches?page=1',
+    ),
+  polls: (pollquery: string) =>
+    fetch_api<ApiPollBundestagData[]>(`bundestag/polls?${pollquery}`),
+  sidejobs: () =>
+    fetch_api<ApiSidejobsBundestag[]>('bundestag/sidejobs?size=5'),
+  donations: () =>
+    fetch_api<ApiBundestagPartyDonation[]>('bundestag/partydonations'),
+};
+
+export const useFetchDashboard = (followIds: Set<number>) => {
+  const pollquery = Array.from(followIds)
+    .map(id => `follow_ids=${id}&`)
+    .join('');
+  return useQueries([
+    {
+      queryKey: 'donations: Bundestag',
+      queryFn: queryFunctions.donations,
+      staleTime: 60 * 10000000, // 10000 minute = around 1 week
+      cacheTime: 60 * 10000000,
+    },
+    {
+      queryKey: 'speeches: Bundestag page:1',
+      queryFn: queryFunctions.speeches,
+      staleTime: 60 * 10000000, // 10000 minute = around 1 week
+      cacheTime: 60 * 10000000,
+    },
+    {
+      queryKey: `polls: Bundestag ${pollquery}`,
+      queryFn: () => queryFunctions.polls(pollquery),
+      staleTime: 60 * 10000000, // 10000 minute = around 1 week
+      cacheTime: 60 * 10000000,
+    },
+    {
+      queryKey: 'sidejobs: Bundestag',
+      queryFn: queryFunctions.sidejobs,
+      staleTime: 60 * 10000000, // 10000 minute = around 1 week
+      cacheTime: 60 * 10000000,
+    },
+  ]);
+};
