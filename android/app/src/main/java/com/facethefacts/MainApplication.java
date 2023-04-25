f3f78901eea4dbcfb6dcf27aae72b86e1480fe1f
Richard <richard.kruemmel@code.berlin>
2023-04-25 21:38:42 +0200

    Delete Flipper as its included in latest RN version

--- android/app/src/main/java/com/facethefacts/MainApplication.java
+++ android/app/src/main/java/com/facethefacts/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -58,6 +59,5 @@ public class MainApplication extends Application implements ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
