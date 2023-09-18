eac1348cd1c6609837f4a8953bc14f40e4dff30c
Richard <richard.kruemmel@code.berlin>
2023-09-18 16:36:45 +0200

    Pass error handling to react-query

--- src/logic/fetch.ts
+++ src/logic/fetch.ts
@@ -20,7 +20,7 @@ export async function fetch_api<T>(url: string): Promise<T | undefined> {
       const jsonResponse = await response.json();
       return jsonResponse as T;
     }
-  } catch (error) {
-    console.error(`error: ${error}`);
+  } catch (error: Error | any) {
+    throw new Error(error);
   }
 }
